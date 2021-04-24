24145e9b907f26695ccf6aad9defb9b1f5b6364c
PowerXWarrior <businessmail1710@mail.ru>
2021-04-24 20:55:55 +0400

    ch

--- DishProject/DishProjectBusinessLogic/BindingModels/OrderBindingModel.cs
+++ DishProject/DishProjectBusinessLogic/BindingModels/OrderBindingModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 using DishProjectBusinessLogic.Enums;
 
 namespace DishProjectBusinessLogic.BindingModels
@@ -6,17 +7,28 @@ namespace DishProjectBusinessLogic.BindingModels
     /// <summary>
     /// Заказ
     /// </summary>
+    [DataContract]
     public class OrderBindingModel
     {
+        [DataMember]
         public int? Id { get; set; }
+        [DataMember]
         public int? ClientId { get; set; }
+        [DataMember]
         public int DishId { get; set; }
+        [DataMember]
         public int Count { get; set; }
+        [DataMember]
         public decimal Sum { get; set; }
+        [DataMember]
         public OrderStatus Status { get; set; }
+        [DataMember]
         public DateTime DateCreate { get; set; }
+        [DataMember]
         public DateTime? DateImplement { get; set; }
+        [DataMember]
         public DateTime? DateFrom { get; set; }
+        [DataMember]
         public DateTime? DateTo { get; set; }
     }
 }
