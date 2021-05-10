4a0e4259d091371021881beeac22f8adbfb1af5e
PowerXWarrior <businessmail1710@mail.ru>
2021-05-10 11:53:36 +0400

    ch

--- DishProject/DishProjectFileImplement/Order.cs
+++ DishProject/DishProjectFileImplement/Order.cs
@@ -9,6 +9,7 @@ namespace DishProjectFileImplement
         public int DishId { get; set; }
         public int? ImplementerId { get; set; }
         public int Count { get; set; }
+        public int ClientId { get; set; }
         public decimal Sum { get; set; }
         public OrderStatus Status { get; set; }
         public DateTime DateCreate { get; set; }
