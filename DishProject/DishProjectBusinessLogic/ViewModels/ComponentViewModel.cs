2867319b25dedfe51c5ac441bf8fab079ab30ee2
PowerXWarrior <businessmail1710@mail.ru>
2021-05-10 13:43:55 +0400

    ch

--- DishProject/DishProjectBusinessLogic/ViewModels/ComponentViewModel.cs
+++ DishProject/DishProjectBusinessLogic/ViewModels/ComponentViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using DishProjectBusinessLogic.Attributes;
 
 namespace DishProjectBusinessLogic.ViewModels
 {
@@ -8,8 +9,10 @@ namespace DishProjectBusinessLogic.ViewModels
     /// </summary>
     public class ComponentViewModel
     {
+        [Column(title: "Номер", width: 100)]
         public int Id { get; set; }
         [DisplayName("Название компонента")]
+        [Column(title: "Название компонента", gridViewAutoSize: GridViewAutoSize.Fill)]
         public string ComponentName { get; set; }
     }
 
--- DishProject/DishProjectBusinessLogic/ViewModels/DishViewModel.cs
+++ DishProject/DishProjectBusinessLogic/ViewModels/DishViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.Serialization;
+using DishProjectBusinessLogic.Attributes;
 
 namespace DishProjectBusinessLogic.ViewModels
 {
@@ -11,12 +12,15 @@ namespace DishProjectBusinessLogic.ViewModels
     public class DishViewModel
     {
         [DataMember]
+        [Column(title: "Номер", width: 100)]
         public int Id { get; set; }
         [DataMember]
         [DisplayName("Название изделия")]
+        [Column(title: "Название изделия", gridViewAutoSize: GridViewAutoSize.Fill)]
         public string DishName { get; set; }
         [DataMember]
         [DisplayName("Цена")]
+        [Column(title: "Цена", width: 50)]
         public decimal Price { get; set; }
         [DataMember]
         public Dictionary<int, (string, int)> DishComponents { get; set; }
