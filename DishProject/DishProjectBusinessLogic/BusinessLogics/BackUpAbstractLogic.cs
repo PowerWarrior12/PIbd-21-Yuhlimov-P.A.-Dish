28ae305bee962417137bc1b9d6a9e4405486daf3
PowerXWarrior <businessmail1710@mail.ru>
2021-05-21 20:10:05 +0400

    change

--- DishProject/DishProjectBusinessLogic/BusinessLogics/BackUpAbstractLogic.cs
+++ DishProject/DishProjectBusinessLogic/BusinessLogics/BackUpAbstractLogic.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
 
 
 namespace DishProjectBusinessLogic.BusinessLogics
@@ -33,7 +35,7 @@ namespace DishProjectBusinessLogic.BusinessLogics
                 var dbsets = GetFullList();
                 // берем метод для сохранения (из базвого абстрактного класса)
                 MethodInfo method =
-               GetType().BaseType.GetTypeInfo().GetDeclaredMethod("SaveToFile");
+               GetType().BaseType.GetTypeInfo().GetDeclaredMethod("SaveToXMLFile");
                 foreach (var set in dbsets)
                 {
                     // создаем объект из класса для сохранения
@@ -64,6 +66,27 @@ namespace DishProjectBusinessLogic.BusinessLogics
            folderName, obj.GetType().Name), FileMode.OpenOrCreate))
             {
                 jsonFormatter.WriteObject(fs, records);
+            }
+        }
+        private void SaveToXMLFile<T>(string folderName) where T : class, new()
+        {
+            var records = GetList<T>();
+            T obj = new T();
+            var typeName = obj.GetType().Name;
+            if (records != null)
+            {
+                var root = new XElement($"{typeName}s");
+                foreach (var record in records)
+                {
+                    var elem = new XElement(typeName);
+                    foreach (var member in obj.GetType().GetMembers().Where(rec => rec.MemberType != MemberTypes.Method && rec.MemberType != MemberTypes.Constructor && !rec.ToString().Contains(".Models.")))
+                    {
+                        elem.Add(new XElement(member.Name, record.GetType().GetProperty(member.Name)?.GetValue(record) ?? "null"));
+                    }
+                    root.Add(elem);
+                }
+                XDocument xDocument = new XDocument(root);
+                xDocument.Save(string.Format("{0}/{1}.xml", folderName, typeName));
             }
         }
         protected abstract Assembly GetAssembly();
